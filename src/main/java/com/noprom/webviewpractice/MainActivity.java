399210325ec3524a2fedd7912a8f9b7202c3d142
noprom <874702417@qq.com>
2014-12-31 06:36:05 +0800

    显示网页数据，展示标题完成
    显示网页数据，展示标题完成

--- src/main/java/com/noprom/webviewpractice/MainActivity.java
+++ src/main/java/com/noprom/webviewpractice/MainActivity.java
@@ -1,39 +1,83 @@
 package com.noprom.webviewpractice;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    // webview
+    private WebView mWebView;
+    // 后退按钮
+    private Button mBackBtn;
+    // 刷新按钮
+    private Button mRefreshBtn;
+    // 标题
+    private TextView mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 初始化控件
+        mWebView = (WebView) findViewById(R.id.webView);
+        mBackBtn = (Button) findViewById(R.id.id_back);
+        mRefreshBtn = (Button) findViewById(R.id.id_refresh);
+        mTitle = (TextView) findViewById(R.id.id_title);
+
+        mWebView.loadUrl("http://www.baidu.com");
+        // 重写方法
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                // 改变标题
+                mTitle.setText(title);
+                super.onReceivedTitle(view, title);
+            }
+        });
+
+        // 设置非浏览器显示
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
+        // 设置监听事件
+        mRefreshBtn.setOnClickListener(new MyListener());
+        mBackBtn.setOnClickListener(new MyListener());
+
     }
 
+    class MyListener implements View.OnClickListener {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                // 刷新
+                case R.id.id_refresh:
+                    mWebView.reload();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    break;
+                // 返回
+                case R.id.id_back:
+                    finish();
+                    break;
+                default:
+                    break;
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
